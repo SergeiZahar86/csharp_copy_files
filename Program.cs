c010bda8510e8486bf3dd4c1f83a0b6a51410864
Сергей Захаров <sergei86zahar@mail.ru>
2021-01-10 19:22:19 +0700

    1

--- Program.cs
+++ Program.cs
@@ -20,8 +20,45 @@ namespace HelloApp
                     Console.WriteLine($"Метка: {drive.VolumeLabel}");
                 }
                 Console.WriteLine();
-                Console.Read();
             }
+            ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            string path = @"C:\SomeDir";
+            string subpath = @"program\avalon";
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+            if (!dirInfo.Exists)
+            {
+                dirInfo.Create();
+            }
+            dirInfo.CreateSubdirectory(subpath);
+            //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            string dirName = "C:\\";
+
+            if (Directory.Exists(dirName))
+            {
+                Console.WriteLine("Подкаталоги:");
+                string[] dirs = Directory.GetDirectories(dirName);
+                foreach (string s in dirs)
+                {
+                    Console.WriteLine(s);
+                }
+                Console.WriteLine();
+                Console.WriteLine("Файлы:");
+                string[] files = Directory.GetFiles(dirName);
+                foreach (string s in files)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+            if (Directory.Exists(path))
+            {
+                Console.WriteLine("Подкаталоги SomeDir");
+                string[] dirs = Directory.GetDirectories(path);
+                foreach (string s in dirs)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+
         }
     }
 }
